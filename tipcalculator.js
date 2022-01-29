bc9958d18e65ac6fb48ef9dd0750e583de23fa05
Web3Learner <programmedhart@protonmail.com>
2022-01-28 21:37:14 -0500

    commit

--- tipcalculator.js
+++ tipcalculator.js
@@ -1,20 +1,37 @@
+/* Declare Query Selectors */
+
 let slider = document.querySelector("#slider");
 let perSpan = document.querySelector("#per");
 let tip = document.querySelector("#tip");
 let price = document.querySelector("#price");
 let total = document.querySelector("#total");
-let slide = 0;
-let initial = 0;
-total.addEventListener("change",function(){
-    
-})
+let sum = 0;
+let slide = "";
+let initial = "";
+let cool = 0;
+
+/* Changes Slider and assigns it's value */
+
 slider.addEventListener("change", function(){
     perSpan.textContent = this.value;
     slide = this.value;
+   
 })
-price.addEventListener("change", function(){
-    initial = this.value;
+
+/* When price is changed Tip and Total listen for the price and slider */
+
+ price.addEventListener("change", function(){
+    initial = parseInt(this.value);
+    slider.addEventListener("change", function(){
+    perSpan.textContent = this.value;
+    slide = parseFloat(this.value);
+    cool = Math.floor((slide / 100 ) * initial);
+    tip.textContent = cool;
+    total.textContent = cool + initial;
 })
+    
+})
+
 
 
 
