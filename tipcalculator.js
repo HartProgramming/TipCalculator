2a2a8080a89892294e163f681f14635fdd3272ba
Web3Learner <programmedhart@protonmail.com>
2022-01-28 19:31:46 -0500

    commit

--- tipcalculator.js
+++ tipcalculator.js
@@ -3,10 +3,18 @@ let perSpan = document.querySelector("#per");
 let tip = document.querySelector("#tip");
 let price = document.querySelector("#price");
 let total = document.querySelector("#total");
-
-slider.addEventListener("change",function(){
-    perSpan.textContent = slider.value;
-    tip.value = (slider.value / price.value) * price.value;
-    total.value += price.value;
+let slide = 0;
+let initial = 0;
+total.addEventListener("change",function(){
+    
 })
+slider.addEventListener("change", function(){
+    perSpan.textContent = this.value;
+    slide = this.value;
+})
+price.addEventListener("change", function(){
+    initial = this.value;
+})
+
+
 
